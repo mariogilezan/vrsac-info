603c4b7304f5ea0f379e14081d9dc8e6377d0f6a
Mario Gilezan <mariogilezan10@gmail.com>
2021-03-24 21:35:05 +0100

    added env check on in gatsby config file to watchchMode

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_TOKEN,
-        watchMode: true,
+        watchMode: process.env.NODE_ENV === "development",
       },
     },
     `gatsby-plugin-material-ui`,
